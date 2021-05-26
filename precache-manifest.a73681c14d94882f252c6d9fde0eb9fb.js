8583c3fc33ce5af74b31b8defd98eeff043c2849
DjevelenDin <freelancer5656@gmail.com>
2021-05-26 16:03:09 +0300

    Updates

---
+++ precache-manifest.a73681c14d94882f252c6d9fde0eb9fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b94115a5fbc2edf09648227d3a4151",
+    "url": "/burger-app/index.html"
+  },
+  {
+    "revision": "c74ac04bf0a03ebe75ab",
+    "url": "/burger-app/static/css/main.948305a7.chunk.css"
+  },
+  {
+    "revision": "8af7ea0ef3c753f69d0a",
+    "url": "/burger-app/static/js/2.8e9e7847.chunk.js"
+  },
+  {
+    "revision": "c74ac04bf0a03ebe75ab",
+    "url": "/burger-app/static/js/main.6bc558ff.chunk.js"
+  },
+  {
+    "revision": "feced66c19dd6f74fe5c",
+    "url": "/burger-app/static/js/runtime-main.18fcb9ae.js"
+  },
+  {
+    "revision": "7497f42a0643b54498a5d553924e817c",
+    "url": "/burger-app/static/media/admin-panel.7497f42a.svg"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/burger-app/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/burger-app/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/burger-app/static/media/up.551b5c6b.svg"
+  }
+]);
